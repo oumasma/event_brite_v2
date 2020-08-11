f1ca724ace7647bf213cfd65a70ae691cabfd0b7
oumasma <oumasma93@hotmail.fr>
2020-08-11 21:48:44 +0400

    home_page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
 class EventsController < ApplicationController
-  def index
+	
+  def index #rechercher les bases de fichier & seed
+    @list_events = Event.all
+  end
+
+  def create
+  end
+
+  def show
+    @event_searched = Event.find(params[:id])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   #get 'events/index'
   devise_for :users
+  
+  resources :users, only: [:show]
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
